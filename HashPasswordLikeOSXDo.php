84ef5d2f604199ee1240c744b5dadebf4e4d3622
TheNewHEROBRINEX <TheNewHEROBRINEX@users.noreply.github.com>
2016-11-09 16:14:09 +0100

    Update HashPasswordLikeOSXDo.php

--- HashPasswordLikeOSXDo.php
+++ HashPasswordLikeOSXDo.php
@@ -1,3 +1,4 @@
+<?php
 $password = "mypassword";
 $iterations = 46728;
 $salt = "mGaAJyk6mVd5qSOs3f/7YAmDwpKLnLoFJIcxG8Lq/dY=";
@@ -5,5 +6,5 @@ $salt = "mGaAJyk6mVd5qSOs3f/7YAmDwpKLnLoFJIcxG8Lq/dY=";
 echo HashPasswordLikeOSXDo($password, $iterations, $salt) . PHP_EOL;
 
 function HashPasswordLikeOSXDo($password, $iterations, $salt) {
-return base64_encode(hex2bin(hash_pbkdf2("sha512", $password, pack('H*', bin2hex(base64_decode($salt))), $iterations, 256));
+  return base64_encode(hex2bin(hash_pbkdf2("sha512", $password, pack('H*', bin2hex(base64_decode($salt))), $iterations, 256));
 }
